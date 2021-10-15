a292d8f486b01dee1f04bf88e53e06bc67a6f38f
gunomnifluencer <gunardi.omnifluencer@gmail.com>
2021-10-15 08:21:58 +0000

    fix-minor-bugs

--- resources/views/emails/NotifyEmail.blade.php
+++ resources/views/emails/NotifyEmail.blade.php
@@ -1,17 +1,25 @@
-Invoice anda : <b>{{ $invoice }}</b><br>
-telah di dispute<br>
+Invoice anda : <b>{{ $invoice }}</b>
+<br>
+telah di dispute
+<br>
 <br>
 
 @if($role == 1)
         Jika anda yakin bahwa anda sudah membayar penjual, maka silahkan menyanggah disini : 
 @else
-        Jika anda belum konfirmasi silahkan konfimasi di sini : <br/><br/>
-        <a href="{!! $url_confirm !!}}">Konfirmasi Penjualan</a> <br/><br/>
+        Jika anda belum konfirmasi silahkan konfimasi di sini : 
+        <br/>
+        <br/>
+        <a href="{!! $url_confirm !!}}">Konfirmasi Penjualan</a>
+        <br/>
+        <br/>
         Jika anda yakin bahwa pembeli belum membayar anda, maka silahkan menyanggah disini :
 @endif
-<br/><br/><a href="{!! $url_dispute !!}}">Dispute</a>
-<br><br>Terima kasih, 
+<br/>
+<br/>
+<a href="{!! $url_dispute !!}}">Dispute</a>
 <br>
-Team Exchanger<br>
-
-
+<br>
+Terima kasih, 
+<br>
+Team Exchanger
